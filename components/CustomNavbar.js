ce38e10a499fbc03d01e1723e4b7bea2bad05505
Matt Frisbie <msfrisbie@gmail.com>
2023-02-04 19:07:46 -0600

    email

--- components/CustomNavbar.js
+++ components/CustomNavbar.js
@@ -1,30 +1,34 @@
+import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 
 export default function CustomNavbar() {
   return (
-    <Navbar bg="light" expand="lg" className="px-3">
-      <Container className="flex-row w-full" style={{ height: "initial" }}>
-        <Navbar.Brand href="/">Matt Frisbie</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/publications">Publications</Nav.Link>
-            <Nav.Link href="/pieces">Less Serious Publications</Nav.Link>
-            <Nav.Link href="https://github.com/msfrisbie" target="_blank">
-              Github
-            </Nav.Link>
-            <Nav.Link href="https://twitter.com/mattfriz" target="_blank">
-              Twitter
-            </Nav.Link>
-            <Nav.Link
-              href="https://www.linkedin.com/in/matt-frisbie-65441513/"
-              target="_blank"
-            >
-              LinkedIn
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar bg="light" expand="lg" className="px-3">
+        <Container className="flex-row w-full" style={{ height: "initial" }}>
+          <Navbar.Brand href="/">Matt Frisbie</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/publications">Publications</Nav.Link>
+              <Nav.Link href="/pieces">Less Serious Publications</Nav.Link>
+              <Nav.Link href="https://github.com/msfrisbie" target="_blank">
+                Github
+              </Nav.Link>
+              <Nav.Link href="https://twitter.com/mattfriz" target="_blank">
+                Twitter
+              </Nav.Link>
+              <Nav.Link
+                href="https://www.linkedin.com/in/matt-frisbie-65441513/"
+                target="_blank"
+              >
+                LinkedIn
+              </Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 }
---
+++ pages/contact.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+
+export default function Contact() {
+  const [showEmail, setShowEmail] = useState(false);
+  const [checked, setChecked] = useState(false);
+
+  const email = atob("bXNmcmlzYmllQGdtYWlsLmNvbQ==");
+
+  return (
+    <>
+      <div className="flex flex-col items-center my-24 gap-8">
+        {!showEmail && (
+          <>
+            <div className="flex flex-row items-center gap-2">
+              <input
+                type="checkbox"
+                checked={checked}
+                onChange={(e) => setChecked(e.target.checked)}
+              />
+              <span>I promise to use this email address only for good</span>
+            </div>
+            {checked && (
+              <Button
+                variant="primary"
+                disabled={!checked}
+                onClick={() => setShowEmail(true)}
+              >
+                GET MATT'S EMAIL
+              </Button>
+            )}
+          </>
+        )}
+        {showEmail && <a href={`mailto:${email}`}>{email}</a>}
+      </div>
+    </>
+  );
+}
