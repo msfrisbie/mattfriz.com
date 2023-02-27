7635c31e5b0766f6a9d2bf8c037ecb3cfa748ea0
Matt Frisbie <msfrisbie@gmail.com>
2023-02-27 15:06:51 -0600

    update contact

--- pages/contact.js
+++ pages/contact.js
@@ -3,39 +3,58 @@ import { Button } from "react-bootstrap";
 
 export default function Contact() {
   const [showEmail, setShowEmail] = useState(false);
-  const [checked, setChecked] = useState(false);
+  const [yesChecked, setYesChecked] = useState(false);
+  const [noChecked, setNoChecked] = useState(false);
 
   const email = atob("bXNmcmlzYmllQGdtYWlsLmNvbQ==");
 
   return (
     <>
-      <div className="flex flex-col items-center my-24 gap-8">
-        {!showEmail && (
-          <>
-            <div className="flex flex-row items-center gap-2">
-              <input
-                id="email"
-                type="checkbox"
-                checked={checked}
-                onChange={(e) => setChecked(e.target.checked)}
-              />
-              <label for="email">
-                I promise to use this email address only for good
-              </label>
-            </div>
-            {checked && (
-              <Button
-                variant="primary"
-                disabled={!checked}
-                onClick={() => setShowEmail(true)}
-              >
-                GET MATT'S EMAIL
-              </Button>
-            )}
-          </>
+      <form className="flex flex-col items-center my-24 gap-8">
+        <div> Do you promise to use this email address only for good?</div>
+
+        <div>
+          <div className="flex flex-row items-center gap-2">
+            <input
+              id="yes"
+              name="group1"
+              type="radio"
+              checked={yesChecked}
+              onChange={(e) => {
+                setYesChecked(e.target.checked);
+                setNoChecked(!e.target.checked);
+              }}
+            />
+            <label htmlFor="yes">Yes</label>
+          </div>
+
+          <div className="flex flex-row items-center gap-2">
+            <input
+              id="no"
+              name="group1"
+              type="radio"
+              checked={noChecked}
+              onChange={(e) => {
+                setNoChecked(e.target.checked);
+                setYesChecked(!e.target.checked);
+              }}
+            />
+            <label htmlFor="no">No</label>
+          </div>
+        </div>
+        {!showEmail && yesChecked && (
+          <Button
+            className="border border-black py-1 px-3 rounded"
+            variant="primary"
+            disabled={!yesChecked}
+            onClick={() => setShowEmail(true)}
+          >
+            GET MATT'S EMAIL
+          </Button>
         )}
-        {showEmail && <a href={`mailto:${email}`}>{email}</a>}
-      </div>
+        {noChecked && <div>Go away.</div>}
+        {showEmail && yesChecked && <a href={`mailto:${email}`}>{email}</a>}
+      </form>
     </>
   );
 }
